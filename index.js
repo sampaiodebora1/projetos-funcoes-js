ac10b4660017083c0769436059531085851d5b52
Débora Sampaio <151951062+sampaiodebora1@users.noreply.github.com>
2026-02-25 16:14:45 -0300

    Add files via upload
    Projeto de funções em JavaScript.

---
+++ index.js
@@ -0,0 +1,15 @@
+function saudacao (nome) {
+    return "Olá, "+ nome +"!";
+}
+console.log (saudacao ("Débora"));
+
+const mensagem = function () {
+    return "Bem-vindos ao meu projeto de funções em JavaScript";
+}
+console.log (mensagem());
+
+const despedida = () => {
+    return "Estudem bastante. Até logo!"
+
+}
+console.log (despedida());
